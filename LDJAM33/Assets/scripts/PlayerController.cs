b29bbe63c29e0fae63ea8355050ab6c18cd56f27
Hamun <commentatorbot@gmail.com>
2015-08-22 16:39:44 +0200

    Playercontroller ikke helt færdig

---
+++ LDJAM33/Assets/scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector2 _force = new Vector2(0,50);
+	private Rigidbody2D _playerRigidbody;
+	private bool _grounded;
+	public GameObject platform;
+
+	public float _speed;
+	// Use this for initialization
+
+	void Start () {
+		player = this.gameObject;
+		_grounded = true;
+		_playerRigidbody = GetComponent<Rigidbody2D> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		player.transform.Translate(Vector2.right * _speed * Time.deltaTime);
+		if (Input.GetKeyDown (KeyCode.Space) && checkJump()) 
+		{
+
+			Jump(_force);
+
+		}
+
+
+	}
+
+	bool checkJump()
+	{
+		//check if the player tag hits the platform tag.
+		return true;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+
+	}
+
+	void Jump(Vector2 force)
+	{
+
+		_playerRigidbody.AddForce (force,ForceMode2D.Impulse);
+	}
+}
