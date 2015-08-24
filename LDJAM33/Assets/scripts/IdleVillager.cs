5492be77517387c6d6838a634db8d02137a03c7b
Nicolai Kirk <Wirdo91@hotmail.com>
2015-08-24 21:05:46 +0200

    no message

---
+++ LDJAM33/Assets/scripts/IdleVillager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleVillager : MonoBehaviour {
+
+    void Start()
+    {
+        Init();
+    }
+
+    int currentAnimation = 0;
+	public void Init () {
+        currentAnimation = new System.Random().Next(0, 3);
+
+        this.GetComponent<Animator>().SetInteger("Animation", currentAnimation);
+	}
+}
