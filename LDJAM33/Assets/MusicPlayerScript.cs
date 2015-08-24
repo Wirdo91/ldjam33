c85c716281f093ae0211369a155753c7fe8b5a3b
Michael Dolatko <sayba_killer@hotmail.com>
2015-08-24 22:17:22 +0200

    Music added Not looping properly as intended yet.

---
+++ LDJAM33/Assets/MusicPlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+	static MusicPlayerScript instance = null;
+	
+	public static MusicPlayerScript Instance
+	{
+		get {return instance;}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartAudio ();
+	}
+
+	void Awake()
+	{
+		if(instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+		}
+		else
+		{
+			instance = this;
+		}
+
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+	void StartAudio()
+	{
+
+	}
+}
